0ceaf73942e3868d52c9a7f7204f1ef176d7f23b
Kent Chrisitan <kentcagadas19@gmail.com>
2025-09-25 18:03:37 +0800

    creatd a User Model, installed msyqlclient, configured database settings

--- accounting/models.py
+++ accounting/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+  ROLE_ADMIN = 'admin'
+  ROLE_ACCOUNTANT = 'accountant'
+  ROLE_CHOICES = [
+    (ROLE_ADMIN, "Admin"),
+    (ROLE_ACCOUNTANT, 'Accountant')
+  ]
+  username = models.CharField(max_length=50)
+  password = models.CharField(max_length=255)
+  role = models.CharField(max_length=15, choices=ROLE_CHOICES, default=ROLE_ACCOUNTANT)
+  email = models.EmailField(max_length=255, unique=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  
--- accounting/views.py
+++ accounting/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import datetime
 
 def test(request):
+
+  today = datetime.datetime.now().date()
  
-  return render(request, "test.html", {})
+  return render(request, "test.html", {"today": today})
