eb8349ea3dd8def7974d765e07a7e534fc7f0a6b
Kent Chrisitan <kentcagadas19@gmail.com>
2025-09-25 18:55:45 +0800

    custom Users: +field-> role (choice), and initial setup

--- accounting/models.py
+++ accounting/models.py
@@ -1,18 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 
-class Users(models.Model):
-  ROLE_ADMIN = 'admin'
-  ROLE_ACCOUNTANT = 'accountant'
+class Users(AbstractUser):
   ROLE_CHOICES = [
-    (ROLE_ADMIN, "Admin"),
-    (ROLE_ACCOUNTANT, 'Accountant')
+    ('admin', 'Admin'),
+    ('accountant', 'Accountant'),
   ]
-  username = models.CharField(max_length=50)
-  password = models.CharField(max_length=255)
-  role = models.CharField(max_length=15, choices=ROLE_CHOICES, default=ROLE_ACCOUNTANT)
-  email = models.EmailField(max_length=255, unique=True)
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
-  
+
+  role = models.CharField(max_length=15, choices=ROLE_CHOICES, default='accountant')
