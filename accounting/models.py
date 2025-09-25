c9cf0096f7eb9ceb4eb469079fd0a2633f1fb713
Kent Chrisitan <kentcagadas19@gmail.com>
2025-09-25 20:17:54 +0800

    created and migrated Model for Accounts

--- accounting/models.py
+++ accounting/models.py
@@ -9,3 +9,28 @@ class Users(AbstractUser):
   ]
 
   role = models.CharField(max_length=15, choices=ROLE_CHOICES, default='accountant')
+
+
+
+
+class Accounts(models.Model):
+  ACCOUNT_TYPE_CHOICES = [
+    ('asset', 'Asset'),
+    ('liability', 'Liability'),
+    ('equity', 'Equity'),
+    ('revenue', 'Revenue'),
+    ('expenses', 'Expenses'),
+  ]
+
+  NORMAL_BALANCE_CHOICES = [
+    ('debit', 'Debit'),
+    ('credit', 'Credit'),
+  ]
+
+  account_name = models.CharField(max_length=50)
+  account_type = models.CharField(max_length=15, choices=ACCOUNT_TYPE_CHOICES)
+  normal_balance = models.CharField(max_length=10, choices=NORMAL_BALANCE_CHOICES)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  
+  
