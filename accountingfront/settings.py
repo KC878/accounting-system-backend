29cacb50bbfc6ffa7d0dacb8833e9164cee1e982
Kent Chrisitan <kentcagadas19@gmail.com>
2025-09-26 22:33:36 +0800

    feat(api): added permission IsAuthenticated, isUserAdmin

--- accountingfront/settings.py
+++ accountingfront/settings.py
@@ -114,6 +114,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
+
 AUTH_USER_MODEL = 'accounting.Users'
 
 # Internationalization
--- api/views.py
+++ api/views.py
@@ -1,9 +1,11 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from accounting.models import Users
 from . serializers import UserSerializer
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated, IsAdminUser])
 def getData(request):
   users = Users.objects.all()
 
@@ -13,6 +15,7 @@ def getData(request):
 
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated, IsAdminUser])
 def addUser(request):
   serializer = UserSerializer(data=request.data)
 
